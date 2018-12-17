1be492d97dfb0590ab0779974348df1dff27fc5d
Chris Gruber <chris@chrisgruber.com>
2018-12-17 00:18:15 -0600

    Add wrapper handler function to log each request received

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+func logRequest(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(fmt.Sprintf("%s request for %s from client address %s", r.Method, r.RequestURI, r.RemoteAddr))
+		h.ServeHTTP(w, r) // call original
+	})
+}
+
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
+	http.Handle("/", logRequest(fs))
 
 	log.Println("http server started on :8181")
 
@@ -16,4 +24,6 @@ func main() {
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+
+	log.Fatal()
 }
